33e5e7d8328e6492bb0855b65b4c6d5475a41729
Vanessa Gutierrez <vanessagutierrez93@msn.com>
2021-04-27 02:23:36 -0500

    Home Layout

---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex max-w-full">
+    <img src="https://via.placeholder.com/200x300.png" alt="Movie Poster" />
+    <div
+      class="flex flex-col justify-between p-3 leading-normal border-b border-l border-r rounded-b bg-gray-200 border-gray-500 lg:border-l-0 lg:border-t lg:border-gray-500 lg:rounded-b-none lg:rounded-r"
+    >
+      <div class="mb-9">
+        <div class="mb-3 text-2xl font-semibold text-red-700 tracking-wider">
+          movie.title
+        </div>
+        <p class="text-amber-600 line-clamp-6">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus
+          modi harum quam porro eaque, totam omnis quod soluta, ab ex saepe
+          consequuntur quas voluptatem ea asperiores dignissimos minus
+          distinctio sed. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Perferendis molestias assumenda totam repudiandae libero
+          mollitia consectetur iusto maiores nobis. Magnam deleniti quos debitis
+          similique dolor alias inventore labore error ipsum.
+        </p>
+      </div>
+      <div class="flex items-start justify-end">
+        <div class="flex flex-col items-center">
+          <button class="text-gray-400 hover:text-red-700">
+            <bi:suit-heart-fill class="w-6 h-6" />
+            <span>3</span>
+          </button>
+        </div>
+        <div class="flex flex-col items-center">
+          <button
+            @click="toggle"
+            class="text-coolGray-400 hover:text-amber-600"
+          >
+            <ic:round-mode-comment class="w-6 h-6" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Comments v-if="value" @close="toggle" />
+</template>
+
+<script setup>
+import { useToggle } from '@vueuse/core'
+
+const [value, toggle] = useToggle(false)
+</script>
---
+++ src/components/Comments.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="fixed inset-0 bg-red-700 bg-center bg-cover"
+    style="
+      background-image: url('https://via.placeholder.com/1920x1080.png?text=MoviesDB+Background+Image');
+    "
+  >
+    <button @click="emit('close')" class="absolute text-gray-400 right-4 top-4">
+      <ant-design:close-circle-twotone
+        class="w-11 h-11 filter drop-shadow-xl"
+      />
+    </button>
+    <div class="inline-block px-3 py-3 mt-10">
+      <h1
+        class="text-5xl font-semibold tracking-wide uppercase text-gray-100 filter drop-shadow-xl"
+      >
+        movie.title
+      </h1>
+    </div>
+    <div
+      class="flex flex-col items-center py-3 space-y-3 overflow-y-scroll h-5/6"
+    >
+      <div
+        v-for="(n, index) in 3"
+        :key="index"
+        class="w-1/2 px-3 py-1 text-left rounded bg-gray-100"
+      >
+        Comment
+        <p class="mt-3 ml-auto text-xs text-right">User</p>
+      </div>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="New Comment"
+        class="w-1/2 rounded bg-gray-100"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineEmit } from 'vue'
+
+const emit = defineEmit(['close'])
+</script>
