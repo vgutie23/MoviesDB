f0736e3cbb0f76178ea51865f6dbcd2c1adb5f66
Vanessa Gutierrez <vanessagutierrez93@msn.com>
2021-04-27 00:33:15 -0500

    NavBar Layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="bg-gray-900">
+    <div class="container flex items-center justify-between mx-auto">
+      <div class="flex items-center space-x-3">
+        <fxemoji:moviecamera class="w-14 h-14" />
+        <p
+          class="text-3xl tracking-wide text-transparent font-light bg-clip-text bg-gradient-to-r from-red-600 to-orange-400"
+        >
+          Extensive<span class="font-semibold">MoviesDB</span>
+        </p>
+      </div>
+      <div>
+        <div v-if="isAuthenticated" class="flex items-center space-x-3">
+          <p class="text-gray-200">Welcome {{ user }}</p>
+          <button
+            @click="signOut"
+            class="px-6 py-2 font-semibold bg-orange-600 rounded-full focus:ring-orange-800 focus:ring-3 focus:outline-none text-gray-200 hover:bg-orange-700"
+          >
+            Logout
+          </button>
+        </div>
+        <div v-else>
+          <button
+            @click="signIn"
+            class="px-6 py-2 font-semibold bg-amber-500 rounded-full focus:ring-amber-700 focus:ring-3 focus:outline-none text-gray-200 hover:bg-amber-600"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isAuthenticated = ref(false)
+const user = ref('')
+
+const signIn = () => {
+  isAuthenticated.value = true
+  user.value = 'Vanessa'
+}
+
+const signOut = () => {
+  isAuthenticated.value = false
+  user.value = ''
+}
+</script>
