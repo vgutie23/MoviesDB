dca14549830a89d7289a75a652c19443a0ab26e2
Vanessa Gutierrez <vanessagutierrez93@msn.com>
2021-04-28 01:44:16 -0500

    Added Comments

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -5,7 +5,7 @@
       background-image: url('${bgBaseURL}${movie.backdrop_path}');
     `"
   >
-    <button @click="emit('close')" class="absolute text-gray-400 right-4 top-4">
+    <button @click="emit('close')" class="absolute text-gray-200 right-4 top-4">
       <ant-design:close-circle-twotone
         class="w-11 h-11 filter drop-shadow-xl"
       />
@@ -21,12 +21,12 @@
       class="flex flex-col items-center py-3 space-y-3 overflow-y-scroll h-5/6"
     >
       <div
-        v-for="(n, index) in 3"
+        v-for="({ text, userName }, index) in comments"
         :key="index"
         class="w-1/2 px-3 py-1 text-left rounded bg-gray-100"
       >
-        Comment
-        <p class="mt-3 ml-auto text-xs text-right">User</p>
+        {{ text }}
+        <p class="mt-3 ml-auto text-xs text-right">{{ userName }}</p>
       </div>
       <input
         type="text"
@@ -34,15 +34,19 @@
         id=""
         placeholder="New Comment"
         class="w-1/2 rounded bg-gray-100"
+        v-model="newComment"
+        @change="add"
       />
     </div>
   </div>
 </template>
 
 <script setup>
-import { defineEmit, defineProps } from 'vue'
+import { defineEmit, defineProps, ref } from 'vue'
 import { bgBaseURL } from '~/helpers/useMovies'
+import { database } from '~/helpers/useFirebase'
 
+const newComment = ref('')
 const emit = defineEmit(['close'])
 
 const props = defineProps({
@@ -56,4 +60,11 @@ const props = defineProps({
     },
   },
 })
+
+const { comments, addComment } = database(props.movie.id)
+
+const add = () => {
+  addComment(newComment.value)
+  newComment.value = ''
+}
 </script>
--- src/helpers/useFirebase.js
+++ src/helpers/useFirebase.js
@@ -1,13 +1,16 @@
+import { ref, onUnmounted } from 'vue'
 import firebase from 'firebase/app'
 import { useAuth } from '@vueuse/firebase'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 import { firebaseConfig } from '~/config/firebase'
 
 firebase.initializeApp(firebaseConfig)
 
-const { auth } = firebase
+const { auth, firestore } = firebase
 const { GoogleAuthProvider } = auth
+const db = firestore()
 
 const { isAuthenticated, user } = useAuth()
 
@@ -15,4 +18,34 @@ export const authentication = () => {
   const signIn = () => auth().signInWithPopup(new GoogleAuthProvider())
   const signOut = () => auth().signOut()
   return { signIn, signOut, isAuthenticated, user }
+}
+
+export const database = movieId => {
+  const comments = ref([])
+
+  const commentsCollection = db.collection('comments')
+  const commentsQuery = commentsCollection
+    .where('movieId', '==', movieId)
+    .orderBy('createdAt')
+
+  const unsubscribe = commentsQuery.onSnapshot(c => {
+    comments.value = c.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+  })
+
+  onUnmounted(unsubscribe)
+
+  const addComment = async text => {
+    if (!isAuthenticated.value) return
+    const { uid, displayName } = user.value
+
+    await commentsCollection.add({
+      userName: displayName,
+      userId: uid,
+      movieId,
+      text,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+  }
+
+  return { comments, addComment }
 }
